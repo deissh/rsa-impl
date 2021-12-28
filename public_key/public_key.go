7a9c676901a08c9f651b7e15c7b0275dcaaedcde
deissh <deissh@yandex.ru>
2021-12-28 19:40:32 +0300

    feat: parse x509 public key

--- public_key/public_key.go
+++ public_key/public_key.go
@@ -1,9 +1,13 @@
 package public_key
 
 import (
-	"crypto/rsa"
-	"crypto/x509"
+	"bytes"
+	"crypto/x509/pkix"
+	"encoding/asn1"
 	"encoding/pem"
+	"errors"
+	"golang.org/x/crypto/cryptobyte"
+	asn1_cb "golang.org/x/crypto/cryptobyte/asn1"
 	"math/big"
 )
 
@@ -12,18 +16,65 @@ type PublicKey struct {
 	E int      // exponent
 }
 
+type publicKeyInfo struct {
+	Raw       asn1.RawContent
+	Algorithm pkix.AlgorithmIdentifier
+	PublicKey asn1.BitString
+}
+
 func FromPEM(raw []byte) (*PublicKey, error) {
 	block, _ := pem.Decode(raw)
-	pub, _ := x509.ParsePKIXPublicKey(block.Bytes)
 
-	decoded, _ := pub.(*rsa.PublicKey)
-
-	key := &PublicKey{decoded.N, decoded.E}
-	if err := checkPub(key); err != nil {
+	var pki publicKeyInfo
+	if rest, err := asn1.Unmarshal(block.Bytes, &pki); err != nil {
+		if _, err := asn1.Unmarshal(block.Bytes, &PublicKey{}); err == nil {
+			return nil, errors.New("x509: failed to parse public key (use ParsePKCS1PublicKey instead for this key format)")
+		}
 		return nil, err
+	} else if len(rest) != 0 {
+		return nil, errors.New("x509: trailing data after ASN.1 of public-key")
 	}
 
-	return key, nil
+	switch getPublicKeyAlgorithmFromOID(pki.Algorithm.Algorithm) {
+	case RSA:
+		der := cryptobyte.String(pki.PublicKey.RightAlign())
+
+		// RSA public keys must have a NULL in the parameters.
+		// See RFC 3279, Section 2.3.1.
+		if !bytes.Equal(pki.Algorithm.Parameters.FullBytes, asn1.NullBytes) {
+			return nil, errors.New("x509: RSA key missing NULL parameters")
+		}
+
+		p := &PublicKey{N: new(big.Int)}
+		if !der.ReadASN1(&der, asn1_cb.SEQUENCE) {
+			return nil, errors.New("x509: invalid RSA public key")
+		}
+		if !der.ReadASN1Integer(p.N) {
+			return nil, errors.New("x509: invalid RSA modulus")
+		}
+		if !der.ReadASN1Integer(&p.E) {
+			return nil, errors.New("x509: invalid RSA public exponent")
+		}
+
+		if p.N.Sign() <= 0 {
+			return nil, errors.New("x509: RSA modulus is not a positive number")
+		}
+		if p.E <= 0 {
+			return nil, errors.New("x509: RSA public exponent is not a positive number")
+		}
+
+		pub := &PublicKey{
+			E: p.E,
+			N: p.N,
+		}
+
+		if err := checkPub(pub); err != nil {
+			return nil, err
+		}
+		return pub, nil
+	default:
+		return nil, errors.New("x509: key unsupported alg")
+	}
 }
 
 // Size returns the modulus size in bytes
